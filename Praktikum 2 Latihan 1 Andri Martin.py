d9baf02fd3d392c57e6e51e83e00bcddc734209b
RexxS8 <andri4martin@gmail.com>
2022-09-23 11:12:41 +0700

    Create Praktikum 2 Latihan 1 Andri Martin.py

---
+++ Praktikum 2 Latihan 1 Andri Martin.py
@@ -0,0 +1,13 @@
+#Andri Martin - 064002200010
+import math
+a = float(input("Masukan Angka A="))
+b = float(input("Masukan Anka B="))
+print("Hasil Penjumlahan=", a+b)
+print("Hasil Pengurangan=", abs(a-b))
+print("Hasil perkalian=", a*b)
+print("Hasil sisa pembagian=", a%b)
+print("Hasil pembagian=", a/b)
+print("Hasil pemangkatan", a**b)
+print("hasil logaritma=", math.log10(a))
+print("Andri Martin - 064002200010")
+print("IG=@adrmrtn")
